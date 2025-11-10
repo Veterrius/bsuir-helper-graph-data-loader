1cd8eed49a395d8586eb4d5c93220d5b618941db
VTR(Konstantin Avrukevich) <avrukievich@gmail.com>
2025-11-10 22:16:42 +0300

    feat: add ollama model setup

--- src/bsuir-helper-graph-data-loader/__main__.py
+++ src/bsuir-helper-graph-data-loader/__main__.py
@@ -1,10 +1,22 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 from .knowledge_base.router import router as kb_router
 from .file.router import router as file_router 
+from .utils import setup_llm_services
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    print("Application startup...")
+    setup_llm_services()
+    
+    yield
+    
+    print("Application shutdown...")
+
+
+app = FastAPI(title="Your Knowledge Base API", lifespan=lifespan)
 app.include_router(kb_router)
 app.include_router(file_router)
 
---
+++ src/bsuir-helper-graph-data-loader/utils.py
@@ -0,0 +1,30 @@
+from llama_index.core import Settings as LlamaSettings # Переименовываем, чтобы не путать
+from llama_index.llms.ollama import Ollama
+from llama_index.embeddings.ollama import OllamaEmbedding
+
+from config import settings
+from organisation_utils.logging_config import logger_factory
+
+logger = logger_factory.get_logger(__name__)
+
+def setup_llm_services():
+
+    logger.info("Setting up LLM and Embedding models...")
+
+    embed_model = OllamaEmbedding(
+        model_name=settings.EMBEDDINGS_MODEL_NAME,
+        base_url=settings.OLLAMA_BASE_URL,
+    )
+    
+    llm = Ollama(
+        model=settings.MODEL_NAME,
+        base_url=settings.OLLAMA_BASE_URL,
+    )
+    
+    LlamaSettings.llm = llm
+    LlamaSettings.embed_model = embed_model
+
+    logger.info(
+        f"LLM ('{settings.MODEL_NAME}') and "
+        f"Embedding model ('{settings.EMBEDDINGS_MODEL_NAME}') have been configured globally."
+    )
