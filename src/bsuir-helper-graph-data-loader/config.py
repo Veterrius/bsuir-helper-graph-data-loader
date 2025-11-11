97bb046ccae987b1813d8cffb3fe138497b3eff5
VTR(Konstantin Avrukevich) <avrukievich@gmail.com>
2025-11-11 12:39:03 +0300

    fix: fix url types

--- src/bsuir-helper-graph-data-loader/config.py
+++ src/bsuir-helper-graph-data-loader/config.py
@@ -1,18 +1,16 @@
 import os
 from pathlib import Path
-from typing import Set
 
+from pydantic import HttpUrl
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-from .models import Extension, Url
 
 class Settings(BaseSettings):
     UPLOAD_DIR: Path
     MAX_FILE_SIZE: int
-    ALLOWED_EXTENSIONS: Set[Extension]
     MODEL_NAME: str
     EMBEDDINGS_MODEL_NAME: str
-    OLLAMA_BASE_URL: Url = Url("http://localhost:11434")
+    OLLAMA_BASE_URL: HttpUrl = HttpUrl("http://localhost:11434")
 
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(os.path.abspath(__file__)), "../.env"),
--- src/bsuir-helper-graph-data-loader/utils.py
+++ src/bsuir-helper-graph-data-loader/utils.py
@@ -1,9 +1,10 @@
 from llama_index.core import Settings as LlamaSettings # Переименовываем, чтобы не путать
 from llama_index.llms.ollama import Ollama
 from llama_index.embeddings.ollama import OllamaEmbedding
-
-from config import settings
 from organisation_utils.logging_config import logger_factory
+
+from .config import settings
+
 
 logger = logger_factory.get_logger(__name__)
 
@@ -13,12 +14,12 @@ def setup_llm_services():
 
     embed_model = OllamaEmbedding(
         model_name=settings.EMBEDDINGS_MODEL_NAME,
-        base_url=settings.OLLAMA_BASE_URL,
+        base_url=str(settings.OLLAMA_BASE_URL),
     )
     
     llm = Ollama(
         model=settings.MODEL_NAME,
-        base_url=settings.OLLAMA_BASE_URL,
+        base_url=str(settings.OLLAMA_BASE_URL),
     )
     
     LlamaSettings.llm = llm
